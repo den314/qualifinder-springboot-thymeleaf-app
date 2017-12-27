ea778cf4782abbd2b4ba3109805cf487b902e03e
Denis <dev.szczukocki.denis@gmail.com>
2017-12-27 11:56:52 +0100

    initial version - dir structure and libs, no code

---
+++ src/main/java/pl/desz/qualifinder/QualifinderApplication.java
@@ -0,0 +1,12 @@
+package pl.desz.qualifinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualifinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualifinderApplication.class, args);
+	}
+}
